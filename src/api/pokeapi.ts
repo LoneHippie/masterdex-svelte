33d0afc809ede6ffe3507d054378aaf1dee4c026
LoneHippie <lonehippie.music@gmail.com>
2022-04-03 22:25:02 +0300

    condensed logic, added more queries

--- src/api/pokeapi.ts
+++ src/api/pokeapi.ts
@@ -13,38 +13,86 @@ const queryOptions = (query: string) => {
     }
 };
 
+const queryBodies = {
+    pokemon_v2: `
+        name
+        id
+        height
+        weight
+        pokemon_v2_pokemonabilities {
+            pokemon_v2_ability {
+                name
+                pokemon_v2_abilityeffecttexts(where: {language_id: {_eq: 9}}) {
+                    short_effect
+                }
+            }
+        }
+        pokemon_v2_pokemonstats {
+            base_stat
+            stat_id
+        }
+        pokemon_v2_pokemontypes {
+            pokemon_v2_type {
+                name
+                id
+            }
+        }
+    `
+}
+
+export function namesQuery() {
+    const namesQuery = `
+        query {
+            pokemon_v2_pokemon {
+                name
+            }
+        }
+    `;
+
+    return pokemonQuery(
+        queryOptions(namesQuery)
+    );
+}
+
+export function searchQuery(search: string) {
+    const searchQuery = `
+        query {
+            pokemon_v2_pokemon(where: {name: {_eq: ${search}}}) {
+                ${queryBodies.pokemon_v2}
+            }
+        }
+    `;
+
+    return pokemonQuery(
+        queryOptions(searchQuery)
+    );
+}
+
 export function genQuery(gen: number) {
     const genQuery = `
         query {
             pokemon_v2_pokemon(where: {pokemon_v2_pokemonspecy: {pokemon_v2_generation: {id: {_eq: ${gen}}}}}) {
-                name
-                id
-                height
-                weight
-                pokemon_v2_pokemonabilities {
-                    pokemon_v2_ability {
-                        name
-                        pokemon_v2_abilityeffecttexts(where: {language_id: {_eq: 9}}) {
-                            short_effect
-                        }
-                    }
-                }
-                pokemon_v2_pokemonstats {
-                    base_stat
-                    stat_id
-                }
-                pokemon_v2_pokemontypes {
-                    pokemon_v2_type {
-                        name
-                        id
-                    }
-                }
+                ${queryBodies.pokemon_v2}
             }
         }
     `;
 
     return pokemonQuery(
         queryOptions(genQuery)
+    );
+}
+
+export function typeQuery(type: string) {
+    const typeQuery = `
+        query {
+            pokemon_v2_pokemon(where: {pokemon_v2_pokemontypes: {pokemon_v2_type: {name: {_eq: ${type}}}}}) {
+                ${queryBodies.pokemon_v2}
+            }
+        }
+    `;
+
+    return pokemonQuery(
+        queryOptions(typeQuery)
     );
 }
 
