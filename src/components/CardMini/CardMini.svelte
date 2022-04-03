de3d11ee8fc33842415895e8f20e36d7af78e5b5
LoneHippie <lonehippie.music@gmail.com>
2022-04-03 22:26:55 +0300

    Added image and more classes

--- src/components/CardMini/CardMini.svelte
+++ src/components/CardMini/CardMini.svelte
@@ -3,21 +3,33 @@
 </script>
 
 <div class="pokemon">
+    <img 
+        src={pokemon.sprite}
+        alt="default portrait"
+        class="pokemon__sprite"
+    />
     <span>{pokemon.name}</span>
 </div>
 
 <style>
     .pokemon {
+        position: relative;
         height: 24rem;
         width: 16rem;
         display: flex;
         flex-direction: column;
         border: 2px solid var(--color-white);
+        padding: 1.5rem;
     }
 
+    .pokemon__sprite {
+        width: 85%;
+        height: 85%;
+    }
     span {
         font-size: 2.2rem;
         margin-top: 3rem;
         text-align: center;
+        height: 7.5%;
     }
 </style>
