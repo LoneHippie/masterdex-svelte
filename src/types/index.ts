07d6ddd0671c5b6e15e960c5ba5fb1b6ae9e0ea8
LoneHippie <lonehippie.music@gmail.com>
2022-04-05 20:47:32 +0300

    Updated types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,29 @@
+interface Effect {
+    shortEffect: string;
+}
+
+interface Ability {
+    name: string;
+    effects: Array<Effect>
+}
+
+interface Stat {
+    baseStat: number;
+    id: number;
+}
+
+interface PkType {
+    name: string;
+    id: number;
+}
+
 export type Pokemon = {
-    abilities: Array<any>;
+    abilities: Array<Ability>;
     height: Number;
     id: Number;
     name: string;
-    stats: Array<any>;
-    types: Array<any>;
+    stats: Array<Stat>;
+    types: Array<PkType>;
     weight: Number;
-    sprite?: string | null;
+    sprite: string | null;
 }
