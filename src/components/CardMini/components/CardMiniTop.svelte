fde24bf8cdb4f25587517c543efb4194e97006fa
LoneHippie <lonehippie.music@gmail.com>
2022-04-09 15:13:22 +0300

    Split UI into mutliple components

---
+++ src/components/CardMini/components/CardMiniTop.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+    import type { Pokemon } from "@typedefs";
+    import { useStyles } from "@hooks";
+
+    export let pokemon: Pokemon;
+
+    const { typeIcons, textColor } = useStyles(pokemon);
+</script>
+
+<section class="pokemon__top">
+    <div class="pokemon__top__icons">
+        {#each typeIcons as typeIcon}
+            <img 
+                src={typeIcon.icon}
+                class="pokemon__top__icons--icon"
+                style="background: {typeIcon.background}"
+                alt="type icon"
+            />
+        {/each}
+    </div>
+    <div 
+        class="pokemon__top--id" 
+        style="color: {textColor}"
+    >
+        #{pokemon.id}
+    </div>
+</section>
+
+<style>
+    .pokemon__top {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        height: 7.5%;
+    }
+    .pokemon__top__icons {
+        display: flex;
+        flex-direction: row;
+        width: 40%;
+        justify-content: space-between;
+    }
+    .pokemon__top__icons--icon {
+        height: 2.75rem;
+        widows: 2.75rem;
+        border-radius: 100%;
+        padding: 0.25rem;
+    }
+    .pokemon__top__icons--icon:first-child {
+        margin-right: 0.35rem;
+    }
+    .pokemon__top--id {
+        font-size: 2.5rem;
+    }
+</style>
