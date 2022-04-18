59fe23714b7b82fd033e9bb80cc2ab8818b2e454
LoneHippie <lonehippie.music@gmail.com>
2022-04-18 21:12:17 +0300

    Refactor to just use store as index for now

--- src/store/index.ts
+++
@@ -1,5 +0,0 @@
-import { writable, Writable } from 'svelte/store';
-import type { Pokemon } from '@typedefs';
-
-export const pokemonList: Writable<Pokemon[]>  = writable([]);
-export const isLoading: Writable<boolean> = writable(false);
